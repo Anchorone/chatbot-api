d022bf23a65bb95b4e76224c984f31900fbe4129
Anchorone <3396851140@qq.com>
2023-04-22 15:21:47 +0800

    push

--- chatbot-api-interfaces/src/test/java/cn/chatbot/api/test/ApiTeat.java
+++ chatbot-api-interfaces/src/test/java/cn/chatbot/api/test/ApiTeat.java
@@ -1,5 +1,59 @@
 package cn.chatbot.api.test;
 
-public class ApiTeat {
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
 
+import java.io.IOException;
+
+public class ApiTeat {
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/88885818842852/topics?scope=unanswered_questions&count=20");
+
+        get.addHeader("cookie","sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%221873d5ed195235-03636145e1ccbae-7a545471-1024000-1873d5ed19681b%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg3M2Q1ZWQxOTUyMzUtMDM2MzYxNDVlMWNjYmFlLTdhNTQ1NDcxLTEwMjQwMDAtMTg3M2Q1ZWQxOTY4MWIifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%221873d5ed195235-03636145e1ccbae-7a545471-1024000-1873d5ed19681b%22%7D; zsxqsessionid=70f1c58a9142e88c148ec1cd85e747b4; abtest_env=product; zsxq_access_token=C3D01E83-34B8-1DB8-049D-9006C97857D9_9F31FDFBAA33A9A3");
+
+        get.addHeader("Content-Type","application/json, text/plain, */*");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("");
+        post.addHeader("cookie","sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%221873d5ed195235-03636145e1ccbae-7a545471-1024000-1873d5ed19681b%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg3M2Q1ZWQxOTUyMzUtMDM2MzYxNDVlMWNjYmFlLTdhNTQ1NDcxLTEwMjQwMDAtMTg3M2Q1ZWQxOTY4MWIifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%221873d5ed195235-03636145e1ccbae-7a545471-1024000-1873d5ed19681b%22%7D; zsxqsessionid=70f1c58a9142e88c148ec1cd85e747b4; abtest_env=product; zsxq_access_token=C3D01E83-34B8-1DB8-049D-9006C97857D9_9F31FDFBAA33A9A3");
+        post.addHeader("Content-Type","application/json, text/plain, */*");
+
+        String paramJson = "";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json","UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+   
 }
