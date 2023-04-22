507142536a22e4edb42038f4f22c17ef8b9ed7fd
Anchorone <3396851140@qq.com>
2023-04-22 11:00:54 +0800

    push

--- chatbot-api-interfaces/src/test/java/cn/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/chatbot/api/test/ApiTest.java
@@ -1,4 +1,32 @@
 package cn.chatbot.api.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
+
 public class ApiTest {
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet();
+
+        get.addHeader("cookie","");
+        get.addHeader("Content-Type","");
+
+        CloseableHttpResponse httpResponse = httpClient.execute(get);
+        if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(httpResponse.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(httpResponse.getStatusLine().getStatusCode());
+        }
+    }
 }
